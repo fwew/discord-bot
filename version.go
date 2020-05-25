d826dfbf987ff2d107d38d742341c47eeb3272f0
knoxfighter <knoxfighter@asdff45.de>
2020-05-25 05:49:57 +0200

    no name for bot :(

--- version.go
+++ version.go
@@ -8,15 +8,13 @@ import (
 type version struct {
 	Major, Minor, Patch int
 	Label               string
-	Name                string
 }
 
 var Version = version{
 	0, 0, 1,
 	"dev",
-	"",
 }
 
 func (v *version) String() string {
-	return fmt.Sprintf("discord bot: %d.%d.%d-%s \"%s\"\n%s", v.Major, v.Minor, v.Patch, v.Label, v.Name, fwew.Version.String())
+	return fmt.Sprintf("discord bot: %d.%d.%d-%s\n%s", v.Major, v.Minor, v.Patch, v.Label, fwew.Version.String())
 }
